979c2542a25e353c4317ef221b166c9e37770531
Nicholas Wood <fortwoodpublishing@gmail.com>
2016-12-28 15:40:58 -0800

    Add Interface Example

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface InterfaceOne
+    {
+        void SayHello();
+    }
+
+    interface InterfaceTwo
+    {
+        void SayGoodbye();
+    }
+
+    class Salutations : InterfaceOne, InterfaceTwo
+    {
+        public void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye.");
+            //throw new NotImplementedException();
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello!");
+            //throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            Salutations cSalut = new Salutations();
+            cSalut.SayHello();
+            cSalut.SayGoodbye();
+
+            Console.ReadLine();
+        }
+    }
+}
