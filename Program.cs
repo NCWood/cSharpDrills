272f466786824d38142f809ee6543ef5944db522
Nicholas Wood <fortwoodpublishing@gmail.com>
2016-12-28 16:38:18 -0800

    Add Try Catch Finally Block

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+
+        public int quotient;
+        
+
+        public void Division(int a, int b)
+        {
+            try
+            {
+                quotient = (a / b);
+            }
+            catch (DivideByZeroException dBz)
+            {
+                Console.WriteLine("Exception caught: {0}", dBz);
+            }
+            finally
+            {
+                Console.WriteLine("Quotient: {0}", quotient);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program cProg = new Program();
+            cProg.Division(12, 0);
+            Console.ReadKey();
+        }
+    }
+
+}
