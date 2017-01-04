293cdac39cc0743cb1ee0b17166308eed90b63ca
Nicholas Wood <fortwoodpublishing@gmail.com>
2017-01-03 19:30:16 -0800

    Add Enumerable example

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> result = from value in Enumerable.Range(0, 10) select value;
+
+
+            foreach (int value in result)
+            {
+                Console.WriteLine(value);
+            }
+
+            double average = result.Average();
+
+            List<int> list = result.ToList();
+            int[] array = result.ToArray();
+        }
+       
+    }
+}
