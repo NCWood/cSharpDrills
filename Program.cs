abf3acda6076f15e6b07f8a721fde8fdb3e15fc1
Nicholas Wood <fortwoodpublishing@gmail.com>
2017-01-03 18:25:19 -0800

    Add multicast delegate example

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void Greetings(string s);
+
+namespace multiCastDelegate
+{
+    class Program
+    {
+
+        static void Hello(string s)
+        {
+            Console.WriteLine(" Hello, {0}", s);
+        }
+
+        static void Goodbye(string s)
+        {
+            Console.WriteLine(" Goodbye, {0}", s);
+        }
+
+        static void Main(string[] args)
+        {
+            Greetings hiGreet, byeGreet, comboGreet, comboLessHiGreet;
+
+            hiGreet = Hello;
+            byeGreet = Goodbye;
+            comboGreet = hiGreet + byeGreet;
+            comboLessHiGreet = comboGreet - hiGreet;
+
+            Console.WriteLine("Invoking delegate hiGreet:");
+            hiGreet("I");
+            Console.WriteLine("Invoking delegate byeGreet:");
+            byeGreet("II");
+            Console.WriteLine("Invoking delegate comboGreet:");
+            comboGreet("III");
+            Console.WriteLine("Invoking delegate comboLessHiGreet:");
+            comboLessHiGreet("IV");
+
+        }
+    }
+}
